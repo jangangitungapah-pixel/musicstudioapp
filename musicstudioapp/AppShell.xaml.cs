44985ad10e3febef35a987fed60697cb215d48e6
hazel <jangangitungapah@gmail.com>
2026-05-19 23:56:27 +0700

    Add project files.

---
+++ musicstudioapp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace musicstudioapp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ musicstudioapp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace musicstudioapp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
